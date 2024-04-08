916e51939d3cf71d62e26ef2584ba2c3483acb09
Simen Tveitane Skogen <simen.skogen@gokstadakademiet.no>
2024-04-08 16:10:58 +0200

    Fetch api with ID and added btn functions

--- js/characterPage.js
+++ js/characterPage.js
@@ -1,8 +1,9 @@
-const URL = "https://swapi.dev/api/people/4";
+const baseURL = "https://swapi.dev/api/people";
 
-async function fetchData() {
+async function fetchData(id) {
+  const completeURL = `${baseURL}/${id}`;
   try {
-    const response = await fetch(URL);
+    const response = await fetch(completeURL);
     if (!response.ok) {
       throw new Error(`Nettside error Status: ${response.status}`);
     }
@@ -15,27 +16,48 @@ async function fetchData() {
 
 async function characterInformation() {
   try {
-    const apiData = await fetchData();
+    const apiData = await fetchData(5);
 
     const pageStructure = document.getElementById("characterPage");
     const characterData = document.getElementById("characterInfo");
     characterData.innerHTML = `
-    <h1>Name: ${apiData.name}</h1>
+    <h1> Character Information </h1>
+    <p>Name: ${apiData.name}</p>
     <p> Gender: ${apiData.gender}</p>
     <p> Height: ${apiData.height} cm</p>
     <p> Hair color: ${apiData.hair_color}</p>
     <p> Eye color: ${apiData.eye_color}</p>
     <p> Year: ${apiData.birth_year}</p>
     `;
+    document.body.style.backgroundColor = "#494D5F";
     pageStructure.style.display = "flex";
+    pageStructure.style.backgroundColor = "#D0BDF4";
+    pageStructure.style.textAlign = "center";
+    pageStructure.style.letterSpacing = "2px";
     pageStructure.style.flexDirection = "column";
     pageStructure.style.justifyContent = "center";
     pageStructure.style.alignItems = "center";
+    pageStructure.style.border = "solid";
+    pageStructure.style.borderWidth = "1.5px";
 
     const personalColletionBtn = document.getElementById(
       "personalCollectionBtn"
     );
-    personalColletionBtn.value = "Here";
+
+    const saveCharacterBtn = document.getElementById("saveBtn");
+    saveCharacterBtn.addEventListener("click", function () {
+      localStorage.setItem("savedCharacter", JSON.stringify(apiData));
+      alert("Character has been saved to collection");
+    });
+    saveCharacterBtn.style.margin = ".5rem";
+
+    const backBtn = document.getElementById("backBtn");
+    backBtn.style.position = "fixed";
+    backBtn.style.top = "5.3%";
+    backBtn.style.left = "30%";
+    backBtn.addEventListener("click", function () {
+      window.history.back();
+    });
   } catch (error) {
     throw new error("Error:", error);
   }
